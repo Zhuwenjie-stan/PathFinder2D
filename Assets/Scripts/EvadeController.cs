7c743d9a5b667245a221ae9b3f03d48dd99fef56
stan <8wy3541223@163.com>
2020-06-12 02:09:06 +0800

    &添加行为
    跟随者不会超过leader&邻居间互斥行为&到达行为&逃离行为

---
+++ Assets/Scripts/EvadeController.cs
@@ -0,0 +1,39 @@
+﻿using Application;
+using UnityEngine;
+//用来防止随机移动的时候超过leader
+public class EvadeController : MonoBehaviour
+{
+
+    private IEntity leader_Locomotion;
+    private IEntity m_pawn;
+    private Vector2 leaderAheadPoint;
+    private float LEADER_BEHIND_DIST;
+    private Vector3 dist;
+    public float evadeDistance;
+    private float sqrEvadeDistance;
+
+    void Start()
+    {
+        LEADER_BEHIND_DIST = 2.0f;
+        sqrEvadeDistance = evadeDistance * evadeDistance;
+    }
+
+
+    void Update()
+    {
+        //计算领队前方的一个点
+        leaderAheadPoint = leader_Locomotion.GetPosition() + leader_Locomotion.GetVelocity().normalized * LEADER_BEHIND_DIST;
+        //计算角色当前位置与领队前方某店的距离，小于某个值，就需要躲避
+        dist = m_pawn.GetPosition() - leaderAheadPoint;
+        if (dist.sqrMagnitude < sqrEvadeDistance)
+        {
+            
+             //enable force
+        }
+        else
+        {
+            
+            //disable force 
+        }
+    }
+}
